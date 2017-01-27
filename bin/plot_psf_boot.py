d07603a466d1adcfaab8e4a5802898708b06d2c1
Julien Guy <jguy@lbl.gov>
2017-01-27 16:52:00 +0100

    astropy

--- bin/plot_psf_boot.py
+++ bin/plot_psf_boot.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import numpy as np
-import pyfits
+#import pyfits
+import astropy.io.fits as pyfits
 import pylab
 from numpy.polynomial.legendre import legval
 import desimodel.io
