a5d01280586f71ce010d73503fa7ce71d1c24d29
Julien Guy <jguy@lbl.gov>
2017-01-30 16:05:13 +0100

    plot arb. number of frames for selected list of fibers. 2D plot orientation consistent with ds9 display of CCD image

--- bin/plot_frame.py
+++ bin/plot_frame.py
@@ -4,19 +4,20 @@
 import sys
 import argparse
 import astropy.io.fits as pyfits
-from teststand.graph_tools         import plot_graph
-from teststand.graph_tools         import show_graph
-from desispec.log import get_logger
+import matplotlib.pyplot as plt
+import numpy as np
+from teststand.graph_tools         import plot_graph,parse_fibers
+from desispec.log                  import get_logger
 import os.path
-
+                
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-f','--frame', type = str, default = None, required = True,
-                    help = 'path of frame fits file')
+parser.add_argument('-f','--frame', type = str, default = None, required = True, nargs="*",
+                    help = 'path to one or several frame fits files')
 parser.add_argument('--fibers', type=str, default = None, required = False,
                     help = 'defines from_to which fiber to work on. (ex: --fibers=50:60,4 means that only fibers 4, and fibers from 50 to 60 (excluded) will be plotted)')
 parser.add_argument('--err', action='store_true', default = None, required = False,
                     help = 'Show errorbar in plot.')
-parser.add_argument('-d', '--dim', action='store_true', default = None, required = False,
+parser.add_argument('--image', action='store_true', default = None, required = False,
                     help = 'Show a 2d graph via imshow plus the actual plot.')
 parser.add_argument('--log', action='store_true', default = False, required = False,
                     help = 'log scale')
@@ -24,34 +25,33 @@ parser.add_argument('-b', '--batch', action='store_true', default = False, requi
                     help = 'batch mode (to save figure and exit')
 parser.add_argument('-o','--output', type = str, default = None, required = False,
                     help = 'save figure in this file')
+parser.add_argument('-l','--legend', action='store_true',help="show legend")
 
 log         = get_logger()
 args        = parser.parse_args()
-frame_file  = pyfits.open(args.frame)
 
-fig=None
-if args.fibers is not None :
-    nb = args.fibers.split(',')
-    for i in range(len(nb)) :
-        if nb[i].isdigit() == False :
-            tmp = nb[i].split(':')
-            if ((len(tmp) is 2) and tmp[0].isdigit() == True and tmp[1].isdigit() == True) :
-                f_begin = int(tmp[0])
-                f_end   = int(tmp[1])
-                plot_graph(frame_file, nfibers=None, start=f_begin, end=f_end, opt_err=args.err, opt_2d=args.dim,logscale=args.log,title=os.path.basename(args.frame))
-            else :
-                log.error("--fibers parsing error.\nCorrect format is either  : --fibers=begin,end (excluded)\nand/or  : --fibers=begin:end (excluded)\nYou can use : --fibers=2,5,6:8,3,10")
-                sys.exit(1)
-        else :
-            nb[i] = int(nb[i])
-            fig=plot_graph(frame_file, nfibers=None, start=nb[i], end=None, only=True, opt_err=args.err, opt_2d=args.dim,logscale=args.log)
-else :
-    #   If you did not ask for a specific fiber/group of fiber, they're all plotted.
-    fig=plot_graph(frame_file, nfibers=frame_file[0].data.shape[0],logscale=args.log,title=os.path.basename(args.frame))
+fig=plt.figure()
+subplot=plt.subplot(1,1,1)
+
+fibers = parse_fibers(args.fibers)
+
+for filename in args.frame :
+    frame_file  = pyfits.open(filename)
+    if fibers is None :
+        fibers = np.arange(frame_file[0].data.shape[0])
+    plot_graph(frame=frame_file,fibers=fibers,opt_err=args.err,opt_2d=args.image,label=os.path.basename(filename))
+
+if args.log :
+    subplot.set_yscale("log")
+if args.legend :
+    subplot.legend(loc="upper left",fontsize="small")
+subplot.grid()
 
 if args.output :
    fig.savefig(args.output)
    log.info("figure saved in %s"%args.output)
+
 if not args.batch :
-    show_graph()
+    plt.show()
+
 
--- py/teststand/graph_tools.py
+++ py/teststand/graph_tools.py
@@ -1,18 +1,34 @@
-import pylab
+import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
 from desispec.log import get_logger
 
-def show_graph() :
-    """
-    Shows plotted graph
-    """
-    pylab.show()
+def parse_fibers(fiber_string) :
+    
+    if fiber_string is None :
+        return None
+    
+    fibers=[]
+    for sub in fiber_string.split(',') :
+        if sub.isdigit() :
+            fibers.append(int(sub))
+            continue
+        
+        tmp = sub.split(':')
+        if ((len(tmp) is 2) and tmp[0].isdigit() == True and tmp[1].isdigit() == True) :
+            for f in range(int(tmp[0]),int(tmp[1])) :
+                fibers.append(f)
+        else :
+            log.error("--fibers parsing error.\nCorrect format is either  : --fibers=begin,end (excluded)\nand/or  : --fibers=begin:end (excluded)\nYou can use : --fibers=2,5,6:8,3,10")
+            sys.exit(1)
+    return np.array(fibers)
 
-def plot_graph(frame, nfibers=None, start=0, end=None, only=False, opt_err=False, opt_2d=False, logscale=False,title=None) :
+
+
+def plot_graph(frame, fibers, opt_err=False, opt_2d=False, label = None, subplot=None) :
     """Plot graph from a given spectra from a fits file and returns figure
-
+    
     ----------
     Parameters
     ----------
@@ -20,85 +36,65 @@ def plot_graph(frame, nfibers=None, start=0, end=None, only=False, opt_err=False
     frame : File Directory
     Where the spectra is collected to be plot.
 
-    nfibers : Optional. If left empty, will set the max number of fibers.
-
-    start   : Optional. If left empty, will be set to 0.
-    end     : Optional. If left empty, will be set to nfibers.
-    Define start/end argument to choose from frame parameter which specific fiber to plot.
-    Example     -> plot_graph(frame, 500, 10, 20) will take 500 fibers into account, and plot from the 10th fiber to the 20th (included).
+    fibers : fibers to show
     """
 
     log         = get_logger()
     spectra     = frame["FLUX"].data
     ivar        = frame["IVAR"].data
     wave        = frame["WAVELENGTH"].data
-    f_shape     = spectra.shape[0]
-    
-    if nfibers is not None :
-        if (nfibers > f_shape) :
-            log.warning("only %d fibers will be shown" % f_shape)
-        f_shape = min(nfibers, f_shape)
+    nfibers     = spectra.shape[0]
 
-    #   If end is not defined, end may take nfibers value
-    #   else it will take max number of fibers value
-    if end is None :
-        end = f_shape
-    elif end > f_shape :
-        log.warning("will plot only %d fibers" % f_shape)
-        end = f_shape
+    if np.max(fibers) >= nfibers :
+        log.warning("requested fiber numbers %s exceed number of fibers in file %d"%(str(fibers),nfibers))
+        fibers=fibers[fibers<nfibers]
     
-    if start > end :
-        log.error("Warning. Nothing will be plotted since begin value is higher than end value")
-    # err = np.sqrt(1./(ivar+(ivar==0)))*(ivar>0)
+    if subplot is None :
+        subplot  = plt.subplot(1,1,1)
     
-    if only == True :
-        end = start + 1
-    
-    fig = pylab.figure()
-    pl  = pylab.subplot(1,1,1)
-    for fiber in range(start, end) :
-        log.info("Plotting fiber %03d" % fiber)
+    for fiber in fibers :
+        
+        if label :
+            fiber_label = "%s Fiber #%d"%(label,fiber)
+        else :
+            fiber_label="Fiber #%d"%fiber
+            
+        log.debug("Plotting fiber %03d" % fiber)
         if opt_err :
             err = np.sqrt(1./ (ivar + (ivar == 0))) * (ivar > 0)
             if len(wave.shape) > 1 :
-                pylab.errorbar(wave[fiber], spectra[fiber], err[fiber], fmt="o-")
+                subplot.errorbar(wave[fiber], spectra[fiber], err[fiber], fmt="o-", label=fiber_label)
             else :
-                pylab.errorbar(wave, spectra[fiber], err[fiber], fmt="o-")
+                subplot.errorbar(wave, spectra[fiber], err[fiber], fmt="o-",label=fiber_label)
         else :
             if len(wave.shape) > 1 :
-                pylab.plot(wave[fiber], spectra[fiber], "-")
+                subplot.plot(wave[fiber], spectra[fiber], "-",label=fiber_label)
             else :
-                pylab.plot(wave, spectra[fiber], "-")
+                subplot.plot(wave, spectra[fiber], "-",label=fiber_label)
     
-    pylab.xlabel("Wavelength [A]")
-    if logscale :
-        log.info("set y log scale")
-        pylab.yscale("log")
-    if title is not None :
-        pl.set_title(title)
+    subplot.set_xlabel("Wavelength [A]")
     
     if opt_2d :
-        pylab.figure()
+        title="spectra"
+        if label is not None:
+            title = label
+        plt.figure(title)
         if len(wave.shape) == 1 :
-            # print(wave[0],wave[-1],start-0.5,end-0.5)
-            # print(spectra[start:end].shape)
-            pylab.imshow(spectra[start:end],
-                        # aspect = spectra.shape[1] / float(end - start),
-                        aspect = 'auto',
-                        extent = (wave[0], wave[-1], start - 0.5, end - 0.5),
-                        origin = 0.,
-                        interpolation = "nearest")
-            pylab.xlabel("Wavelength [A]")
-            pylab.ylabel("Fiber #")
+            plt.imshow(spectra[fibers].T,
+                       aspect = 'auto',
+                       extent = (fibers[0] - 0.5, fibers[-1] + 0.5, wave[0], wave[-1]),
+                       origin = 0.,
+                       interpolation = "nearest")
+            plt.ylabel("Wavelength [A]")
+            plt.xlabel("Fiber #")
         else :
-            pylab.imshow(spectra[start:end],
-                        # aspect = spectra.shape[1] / float(end - start),
-                        aspect = 'auto',
-                        extent = (0,spectra.shape[1],start-0.5,end-0.5),
-                        origin = 0.,
-                        interpolation = "nearest")
-            pylab.xlabel("Y CCD")
-            pylab.ylabel("Fiber #")
-        pylab.colorbar()
+            plt.imshow(spectra[fibers].T,
+                       aspect = 'auto',
+                       extent = (fibers[0]-0.5, fibers[-1]+0.5, 0,spectra.shape[1]),
+                       origin = 0.,
+                       interpolation = "nearest")
+            plt.ylabel("Y CCD")
+            plt.xlabel("Fiber #")
+        plt.colorbar()
     
-    return fig       
+
