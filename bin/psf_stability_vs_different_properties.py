0256ebc0c02798796e75285fcf95922108f9b807
Data Systems <datasystems@desi-10.kpno.noao.edu>
2019-03-07 22:43:39 -0700

    modify name psf_variability and update to be faster and add more possible x-axis

--- bin/psf_stability_vs_different_properties.py
+++ bin/psf_stability_vs_different_properties.py
@@ -10,18 +10,20 @@ import argparse
 from astropy.time import Time
 
 from desispec.io.xytraceset import read_xytraceset
+from desispec.calibfinder import CalibFinder
 
+def preproc(indir,outdir,cam,idx):
 
-def preproc(indir,outdir,cam):
-
+    isDone = True
+    for tc in cam:
+        if not os.path.isfile('{}/preproc-{}-{}.fits'.format(outdir,tc,idx)):
+            isDone = False
+    if isDone:
+        print('INFO: {} already done'.format(idx))
+        return
 
     cmd = 'desi_preproc'
     cmd += ' -i {}'.format(indir)
-    cmd += ' --cameras '
-    for i,tc in enumerate(cam):
-        cmd += tc
-        if i!=len(cam)-1:
-            cmd += ','
     cmd += ' --outdir {}'.format(outdir)
     print(cmd)
     subprocess.call(cmd,shell=True)
@@ -33,9 +35,15 @@ def psf_fit(idx,outdir,cam):
         print('INFO: already done')
         return
 
+    h = fitsio.FITS('{}/preproc-{}-{}.fits'.format(outdir,cam,idx))
+    head = h['IMAGE'].read_header()
+    cfinder = CalibFinder([head])
+    p = cfinder.findfile('PSF')
+    h.close()
+
     cmd = 'desi_psf_fit'
     cmd += ' -a {}/preproc-{}-{}.fits'.format(outdir,cam,idx)
-    cmd += ' --in-psf /software/datasystems/desi_spectro_calib/trunk/spec/sp2/psf-{}-20190301.fits'.format(cam)
+    cmd += ' --in-psf {}'.format(p)
     cmd += ' --out-psf {}/psf-{}-{}.fits'.format(outdir,cam,idx)
     if 'b' in cam:
         cmd += ' --trace-deg-wave 4 --trace-deg-x 4 --legendre-deg-x 4 --legendre-deg-wave 2 --single-bundle'
@@ -98,7 +106,7 @@ def plot_resolution(outdir,cam,start,end,exptime):
     subprocess.call(cmd,shell=True)
 
     return
-def plot_psf_vs_time(outdir,cam,exps,fiber,dicLines):
+def plot_psf_vs_time(outdir,cam,exps,fiber,dicLines,xaxis):
 
     dic = copy.deepcopy(exps)
     for k,v in dic.items():
@@ -108,7 +116,7 @@ def plot_psf_vs_time(outdir,cam,exps,fiber,dicLines):
         try:
             psf = read_xytraceset(p)
         except:
-            print('\nERROR',p,'\n')
+            print('INFO: did not find: ',p)
             continue
         dic[k][cam]['SIGMAX'] = psf.xsig_vs_wave(fiber,dicLines[cam[0]]['LINE'])
         dic[k][cam]['SIGMAY'] = psf.ysig_vs_wave(fiber,dicLines[cam[0]]['LINE'])
@@ -121,24 +129,32 @@ def plot_psf_vs_time(outdir,cam,exps,fiber,dicLines):
         plt.subplots_adjust(top=0.95,hspace=0.,wspace=0.)
         plt.suptitle(r'$\mathrm{SP'+str(cam[-1])+', cam = '+cam+',\, exptime='+str(exptime)+'}$',fontsize=20)
         for i,k in enumerate(['SIGMAX','SIGMAY','X','Y']):
-            x = sp.array([ dic[tk]['DATE_OBS'] for tk in dic.keys() if dic[tk]['EXPTIME']==exptime and k in dic[tk][cam].keys() ])
+
             y = sp.array([ dic[tk][cam][k] for tk in dic.keys() if dic[tk]['EXPTIME']==exptime and k in dic[tk][cam].keys() ])
-            w = sp.argsort(x)
-            x = x[w]
-            y = y[w]
-            x *= 24.*3600.
-            x -= x[0]
-            y = 100.*(y-y[0])/y[0]
-            ax[i].plot(x,y,'o-')
+            if y.size==0: continue
+            if xaxis=='DATE_OBS':
+                x = sp.array([ dic[tk]['DATE_OBS'] for tk in dic.keys() if dic[tk]['EXPTIME']==exptime and k in dic[tk][cam].keys() ])
+                w = sp.argsort(x)
+                x = x[w]
+                y = y[w]
+                x *= 24.*3600.
+                x -= x[0]
+                linemarker = 'o-'
+            else:
+                x = sp.array([ dic[tk]['CAM'][cam][xaxis] for tk in dic.keys() if dic[tk]['EXPTIME']==exptime and k in dic[tk][cam].keys() ])
+                linemarker = 'o'
+            y -= y[0]
+
+            ax[i].plot(x,y,linemarker)
             if i==len(['SIGMAX','SIGMAY','X','Y'])-1:
-                ax[i].set_xlabel(r'$\mathrm{\Delta DATE-OBS} \, [\mathrm{s}]$')
+                ax[i].set_xlabel(r'$\mathrm{'+xaxis.replace('_','')+'}$')
             else:
                 ax[i].set_xticklabels([])
             ax[i].grid()
-            ax[i].set_ylabel(r'$\mathrm{'+k+'} \, [\%]$')
+            ax[i].set_ylabel(r'$\mathrm{'+k+'} \, [\mathrm{pix}]$')
 
         #plt.show()
-        plt.savefig(outdir+'/psf-vs-time-cam-{}-exptime-{}.png'.format(cam,exptime))
+        plt.savefig('{}/psf-vs-{}-cam-{}-exptime-{}.png'.format(outdir,xaxis,cam,exptime))
         plt.clf()
 
     return
@@ -177,6 +193,18 @@ def main():
 
     parser.add_argument('--more-plot', action='store_true',
         help="A lot of plot of control")
+
+    parser.add_argument('--x-axis', type=str, default='DATE_OBS', required=False,
+        help='Plot the different quatity versus x-axis in {DATE_OBS,TEMP,HUMID}')
+
+    parser.add_argument('--b-exp-time', type=float, default=10., required=False,
+        help='Exposure time for b')
+
+    parser.add_argument('--r-exp-time', type=float, default=1., required=False,
+        help='Exposure time for r')
+
+    parser.add_argument('--z-exp-time', type=float, default=2., required=False,
+        help='Exposure time for z')
     
     parser.add_argument('--b-line-value', type=float, default=4359.56, required=False,
         help='Value of the emission line for the b-camera')
@@ -197,11 +225,14 @@ def main():
         help='Name of the emission line for the z-camera')
     
     args = parser.parse_args()
-    
+
     ###
     dicLines = {'b':{}, 'r':{}, 'z':{}}
     for c in dicLines.keys():
         dicLines[c] = {'ELE':getattr(args,'{}_line_name'.format(c)), 'LINE':getattr(args,'{}_line_value'.format(c))}
+
+    ###
+    from_cam_to_name = {'b':'BLUE', 'r':'RED','z':'NIR'}
 
     ###
     cam = args.cameras
@@ -218,26 +249,43 @@ def main():
         h = fitsio.FITS(tindir)
         exps[k]['DATE_OBS'] = Time(h['SPS'].read_header()['DATE-OBS']).mjd
         exps[k]['EXPTIME'] = h['SPS'].read_header()['EXPTIME']
+
+        temp = h['SPECTCONS'].read_header()['DEVICES'].replace(' ','').split(',')
+        from_spectro_to_indx = { el[-1]:i for i,el in enumerate(temp) }
+
+        exps[k]['CAM'] = { c:{} for c in cam }
+        for tk in ['TEMP','HUMID']:
+            for c in cam:
+                try:
+                    q = h['SPECTCONS'][from_cam_to_name[c[0]]+tk][:]
+                except:
+                    q = h['SPECTCONS'][from_cam_to_name[c[0]][:-1]+tk][:]
+                exps[k]['CAM'][c][tk] = q[from_spectro_to_indx[c[-1]]]
         h.close()
+    exps = { k:exps[k] for k in exps.keys() if exps[k]['EXPTIME']>0. }
 
     ###
     if args.run_preproc:
         for i in exps.keys():
             idx = str(i).zfill(8)
             tindir = indir+'/{0}/desi-{0}.fits.fz'.format(idx)
-            preproc(tindir,outdir,cam)
+            preproc(tindir,outdir,cam,idx)
 
     ###
     if args.run_psffit:
-        for i in exps.keys():
-            for tcam in cam:
+        for tcam in cam:
+            for i in exps.keys():
+                if exps[i]['EXPTIME']!=getattr(args,'{}_exp_time'.format(tcam[0])):
+                    print('INFO: not running ', i, tcam, exps[i]['EXPTIME'])
+                    continue
+                print(i, tcam, exps[i]['EXPTIME'])
                 idx = str(i).zfill(8)
                 psf_fit(idx,outdir,tcam)
 
     ###
     if args.plot:
         for tcam in cam:
-            plot_psf_vs_time(outdir,tcam,exps,args.fiber,dicLines)
+            plot_psf_vs_time(outdir,tcam,exps,args.fiber,dicLines,args.x_axis)
 
     ###
     if args.more_plot:
